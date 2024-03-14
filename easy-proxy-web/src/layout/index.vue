5b01ba0d17c359d57bc0745a58ca20e80158273c
chen.keyu <chen.keyu@darcy.com>
2024-03-14 23:26:55 +0800

    feat: 添加布局

--- easy-proxy-web/src/layout/index.vue
+++ easy-proxy-web/src/layout/index.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import {ElContainer, ElFooter, ElHeader} from 'element-plus'
+import {ElAside, ElContainer, ElHeader} from 'element-plus'
 import {computed} from "vue";
 import {useRoute} from "vue-router";
 
@@ -13,32 +13,54 @@ const key = computed(() => {
 
 <template>
   <div class="app-container">
-    <el-container>
+    <el-container style="min-height: 100vh;">
       <el-header class="header">Header</el-header>
-      <el-main class="main">
-        <router-view :key="key"/>
-      </el-main>
-      <el-footer class="footer">Footer</el-footer>
+      <el-header class="header header-fix">Header</el-header>
+      <el-container style="display: flex; flex-direction: row; position: relative;">
+        <el-aside class="aside">Aside</el-aside>
+        <el-aside class="aside aside-fix">Aside</el-aside>
+        <el-main class="main">
+          <router-view :key="key"/>
+        </el-main>
+      </el-container>
     </el-container>
   </div>
 </template>
 
 <style lang="scss">
+.app-container {
+  min-height: 100vh;
+}
+
 .header {
   background-color: var(--el-color-primary-light-7);
+  height: 60px;
+  line-height: 60px;
+  width: 100%;
+}
+
+.header-fix {
+  position: fixed;
+  top: 0;
+  z-index: 100;
 }
 
 .aside {
+  width: 200px;
   background-color: var(--el-color-primary-light-8);
 }
 
+.aside-fix {
+  position: fixed;
+  top: 60px;
+  left: 0;
+  bottom: 0;
+}
+
 .main {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
   background-color: var(--el-color-primary-light-9);
-  height: 150px;
 }
-
-.footer {
-  background-color: var(--el-color-primary-light-7);
-}
-
 </style>
