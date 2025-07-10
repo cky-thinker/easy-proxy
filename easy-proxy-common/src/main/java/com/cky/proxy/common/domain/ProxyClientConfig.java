75ae51f04731dfa3e61320370337f875dfc4de16
陈科宇 <759891347@qq.com>
2025-07-10 09:14:13 +0800

    feat: 配置项修改

--- easy-proxy-common/src/main/java/com/cky/proxy/common/domain/ProxyClientConfig.java
+++ easy-proxy-common/src/main/java/com/cky/proxy/common/domain/ProxyClientConfig.java
@@ -6,7 +6,28 @@ import java.util.List;
 
 @Data
 public class ProxyClientConfig {
+    /**
+     * 客户端名称
+     */
     private String name;
+    /**
+     * token
+     */
     private String token;
+    /**
+     * 在线状态 online offline
+     */
+    private String status;
+    /**
+     * 使用流量
+     */
+    private long usedTraffic;
+    /**
+     * 启用标记
+     */
+    private Boolean enableFlag;
+    /**
+     * 转发规则
+     */
     private List<ProxyRule> proxyRules;
 }
--- easy-proxy-common/src/main/java/com/cky/proxy/common/domain/ProxyRule.java
+++ easy-proxy-common/src/main/java/com/cky/proxy/common/domain/ProxyRule.java
@@ -4,7 +4,20 @@ import lombok.Data;
 
 @Data
 public class ProxyRule {
+    /**
+     * 规则名称
+     */
     private String name;
+    /**
+     * 服务端监听端口
+     */
     private int serverPort;
+    /**
+     * 客户端转发地址，格式为 ip:port
+     */
     private String clientAddress;
+    /**
+     * 启用标记
+     */
+    private Boolean enableFlag;
 }
