50ad68ebe441b4f0ca3bbf00fb7b43e9b133c235
陈科宇 <759891347@qq.com>
2025-07-11 09:58:15 +0800

    页面布局调整

--- easy-proxy-web/src/App.vue
+++ easy-proxy-web/src/App.vue
@@ -1,85 +1,38 @@
 <script setup lang="ts">
-import {RouterLink, RouterView} from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
     <header>
-        <div class="wrapper">
-            <HelloWorld msg="You did it!"/>
-
-            <nav>
-                <RouterLink to="/">Home</RouterLink>
-                <RouterLink to="/about">About</RouterLink>
-                <RouterLink to="/proxy">代理管理</RouterLink>
-                <RouterLink to="/tailwind">Tailwind示例</RouterLink>
-            </nav>
-        </div>
+        <span class="logo"></span>
+        <span class="title"></span>
     </header>
+    <div>
+        <aside>
+            <div class="menu">
+                <div class="item"><RouterLink to="/">Home</RouterLink></div>
+                <div class="item"><RouterLink to="/about">About</RouterLink></div>
+                <div class="item"><RouterLink to="/proxy">代理管理</RouterLink></div>
+                <div class="item"><RouterLink to="/tailwind">Tailwind示例</RouterLink></div>
+            </div>
+        </aside>
+        <div>
+            <main>
+                <RouterView />
+            </main>
+            <footer>
+                <div>
+                    <p>
+                        Copyright &copy; 2023
+                    </p>
+                </div>
+            </footer>
+        </div>
+    </div>
 
-    <RouterView/>
+    
 </template>
 
 <style scoped>
-header {
-    line-height: 1.5;
-    max-height: 100vh;
-}
 
-.logo {
-    display: block;
-    margin: 0 auto 2rem;
-}
-
-nav {
-    width: 100%;
-    font-size: 12px;
-    text-align: center;
-    margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-    color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-    background-color: transparent;
-}
-
-nav a {
-    display: inline-block;
-    padding: 0 1rem;
-    border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-    border: 0;
-}
-
-@media (min-width: 1024px) {
-    header {
-        display: flex;
-        place-items: center;
-        padding-right: calc(var(--section-gap) / 2);
-    }
-
-    .logo {
-        margin: 0 2rem 0 0;
-    }
-
-    header .wrapper {
-        display: flex;
-        place-items: flex-start;
-        flex-wrap: wrap;
-    }
-
-    nav {
-        text-align: left;
-        margin-left: -1rem;
-        font-size: 1rem;
-
-        padding: 1rem 0;
-        margin-top: 1rem;
-    }
-}
 </style>
--- easy-proxy-web/src/router/index.ts
+++ easy-proxy-web/src/router/index.ts
@@ -1,33 +1,24 @@
-import {createRouter, createWebHistory} from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-        name: 'home',
-        component: HomeView,
-    },
-      {
-          path: '/about',
-          name: 'about',
-          // route level code-splitting
-          // this generates a separate chunk (About.[hash].js) for this route
-          // which is lazy-loaded when the route is visited.
-          component: () => import('../views/AboutView.vue'),
-      },
-      {
-          path: '/proxy',
-          name: 'proxy',
-          component: () => import('../views/ProxyView.vue'),
-      },
-      {
-          path: '/tailwind',
-          name: 'tailwind',
-          component: () => import('../views/TailwindView.vue'),
-      },
-  ],
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: () => import('../views/TailwindView.vue'),
+        },
+        {
+            path: '/proxy',
+            name: 'proxy',
+            component: () => import('../views/ProxyView.vue'),
+        },
+        {
+            path: '/tailwind',
+            name: 'tailwind',
+            component: () => import('../views/TailwindView.vue'),
+        },
+    ],
 })
 
 export default router
