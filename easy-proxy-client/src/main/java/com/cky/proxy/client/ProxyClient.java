8d4a20c6908c8430c06cc837213b81b417e6a471
陈科宇 <759891347@qq.com>
2026-03-17 16:22:33 +0800

    feat: proxyclient添加初始化操作

--- easy-proxy-client/src/main/java/com/cky/proxy/client/ProxyClient.java
+++ easy-proxy-client/src/main/java/com/cky/proxy/client/ProxyClient.java
@@ -3,12 +3,16 @@ package com.cky.proxy.client;
 import com.cky.proxy.client.util.CertDownloader;
 import com.cky.proxy.client.verticle.MainVerticle;
 
+import com.cky.proxy.common.util.ConfigBootstrap;
+
 import io.vertx.core.Vertx;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class ProxyClient {
     public static void main(String[] args) {
+        // 检查并复制默认配置文件
+        ConfigBootstrap.initConfigs();
         // 检查并下载证书：不存在则下载证书
         try {
             CertDownloader.downloadIfNotExists();
